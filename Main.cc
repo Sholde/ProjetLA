834259c6e71c2be81418122d070634f90658eca2
Sholde <exshode@gmail.com>
2019-12-17 23:01:25 +0100

    test

--- Main.cc
+++ Main.cc
@@ -4,11 +4,24 @@ int main()
 {
   sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
   sf::CircleShape shape(100.f);
+  int x = 1;
   shape.setFillColor(sf::Color::Green);
+  
+  int i = 0;
 
   while (window.isOpen())
   {
+    i++;
     sf::Event event;
+    if(! (i%6)) {
+      if(shape.getPosition().x > 100) {
+        x = -1;
+      }
+      if(shape.getPosition().x < -100) {
+        x = 1;
+      }
+      shape.setPosition(shape.getPosition().x + x, shape.getPosition().y);
+    }
     while (window.pollEvent(event))
       {
         if (event.type == sf::Event::Closed)
