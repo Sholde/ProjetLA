9041d9e6a55db4c92a93f94d7648143520455e18
Sholde <exshode@gmail.com>
2020-01-15 21:51:42 +0100

    petit script

--- config.sh
+++ config.sh
@@ -1,2 +1,16 @@
-mkdir bin
-mkdir obj
+#!/bin/bash
+
+'pwd renvoie le répertoire courant'
+'## pour pouvoir écrire des chose apres'
+
+'test si les répertoires existe et les créer sinon'
+
+if [ -d ${pwd##/bin} ];then
+	echo "Dossier déjà existant";
+	else mkdir bin
+fi
+
+if [ -d ${pwd##/obj} ];then
+	echo "Dossier déjà existant";
+	else mkdir obj
+fi
