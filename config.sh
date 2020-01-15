42f959916647c0df104ac2919a96581b59e6dcfc
Sholde <exshode@gmail.com>
2020-01-15 21:54:28 +0100

    commentaire

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-'pwd renvoie le répertoire courant'
-'## pour pouvoir écrire des chose apres'
+# pwd renvoie le répertoire courant
+# ## pour pouvoir écrire des chose après
 
-'test si les répertoires existe et les créer sinon'
+# test si les répertoires existe et les créer sinon
 
-if [ -d ${pwd##/bin} ];then
-	echo "Dossier déjà existant";
+if [ -d ${pwd##/bin} ];
+	then echo "Dossier déjà existant";
 	else mkdir bin
 fi
 
-if [ -d ${pwd##/obj} ];then
-	echo "Dossier déjà existant";
+if [ -d ${pwd##/obj} ];
+	then echo "Dossier déjà existant";
 	else mkdir obj
 fi
